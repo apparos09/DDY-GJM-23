1c0878f256cef1936aca281ba7ec1dc22088ca0c
Roderick Montague <roderick.montague243@gmail.com>
2023-05-25 12:35:26 -0400

    Going to Test Area Loader
    Going to test the area loader now to see if everything worked properly.

--- Assets/Scripts/AreaLoader.cs
+++ Assets/Scripts/AreaLoader.cs
@@ -11,34 +11,62 @@ namespace DDY_GJM_23
     // Loads an area from a file.
     public class AreaLoader : MonoBehaviour
     {
+        // Loads the data on start.
+        public bool loadOnStart = true;
+
         // The world sector.
         public WorldArea.worldSector sector = WorldArea.worldSector.unknown;
 
         // The number of the area.
         public int areaNumber = -1;
 
+        // The reference position for loaded content.
+        [Tooltip("The offset of the tile positions.")]
+        public Vector3 posOffset = new Vector3(-28, 14, 0);
+
+        // The spacing for placing tiles.
+        [Tooltip("Spacing for tiles [x = col, y = row]")]
+        public Vector2 spacing = new Vector2(1, -1);
+
         [Header("Files")]
 
         // The file name (include the file extension).
+        [Tooltip("The file name. Make sure to include the file extension (.csv).")]
         public string fileName = "";
 
         // The three folder paths that are used to check for files.
         // Do NOT have them end in a slash.
+        [Tooltip("The primary search location. Include the slash at the end of the path.")]
         public string folderPath1 = "Assets/Resources/Data/Areas/";
+
+        [Tooltip("The secondary search location. Include the slash at the end of the path.")]
         public string folderPath2 = "Resources/Data/Areas/";
+
+        [Tooltip("The tertiary search location. Include the slash at the end of the path.")]
         public string folderPath3 = "Data/Areas/";
+
 
         [Header("Parents")]
         // The parent for the loaded tiles.
         public Transform tilesParent = null;
 
+        [Header("Prefabs")]
+
+        // GRASS
+        public WorldTile grassFloorA;
+
+        // METAL
+        public WorldTile metalFloorA;
+
         // Start is called before the first frame update
         void Start()
         {
+            if (loadOnStart)
+                LoadData();
         }
 
         // Loads the data from the set file.
-        private bool LoadData()
+        public bool LoadData()
         {
             // The data file being loaded.
             string file = "";
@@ -123,7 +151,54 @@ namespace DDY_GJM_23
                 // 1 - Tiles
                 else
                 {
-                    // TODO: read tile code. 
+                    // Goes through each entry.
+                    for(int j = 0; j < entries.Length; j++)
+                    {
+                        // Gets the tile code.
+                        string tc = entries[j];
+
+                        // If the tile code is empty, is set to "0" or "00A", nothing should be put here.
+                        // So go onto the next entry.
+                        if(tc.Length != 3 || tc == "00A")
+                            continue;
+
+                        // The first 2 characters are the number, and the third character is the type.
+                        int number;
+                        string type;
+
+                        // Converts the tile number from a string to an int.
+                        int.TryParse(tc.Substring(0, 2), out number);
+
+                        // Gets the type as a string.
+                        type = tc.Substring(2, 1);
+
+
+                        // TILE LOADING //
+
+                        // Instantiates the tile.
+                        WorldTile newTile = InstantiateTile(number, type[0]);
+
+                        // No tile was generated, so continue.
+                        if (newTile == null)
+                            continue;
+
+
+                        // TILE POSITIONING //
+
+                        // The tile position.
+                        Vector3 tilePos = posOffset;
+                        
+                        // The row and column. Var 'col' is -1 since the first line was the area ID.
+                        int row = j;
+                        int col = i - 1;
+
+                        // Calculate the tile position.
+                        tilePos.x += spacing.x * col;
+                        tilePos.y += spacing.y * row;
+
+                        // Give it the tiles parent.
+                        newTile.transform.parent = tilesParent;
+                    } 
                 }
             }
 
@@ -131,10 +206,54 @@ namespace DDY_GJM_23
             return true;
         }
 
-        // Update is called once per frame
-        void Update()
+        // Instantiates a tile.
+        public WorldTile InstantiateTile(int number, char type)
         {
+            // The original tile.
+            WorldTile origTile = null;
 
+            // The instantiated tile.
+            WorldTile copyTile;
+
+            // Checking the tile number.
+            switch(number)
+            {
+                case 1: // Grass
+                    // Checking the tile type.
+                    switch (type)
+                    {
+                        case 'A':
+                        case 'a':
+                        default:
+                            origTile = grassFloorA;
+                            break;
+                    }
+
+                    break;
+                case 2: // Metal
+                    // Checking the tile type.
+                    switch (type)
+                    {
+                        case 'A':
+                        case 'a':
+                        default:
+                            origTile = metalFloorA;
+                            break;
+                    }
+                    break;
+            }
+
+            // Checks if the original tile was set or not.
+            if(origTile != null) // Set
+            {
+                // Copies the tile and returns it.
+                copyTile = Instantiate(origTile);
+                return copyTile;
+            }
+            else // Not set.
+            {
+                return null;
+            }
         }
     }
 }

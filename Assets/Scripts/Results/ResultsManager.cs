30098302734344ecfff2ed990480a438489663ad
mecha-rm <roderick.montague243@gmail.com>
2023-05-26 19:14:11 -0400

    Added Some Title Screen Content
    Added some information to the title screen. I need to set up the results screen as well.

--- Assets/Scripts/Results/ResultsManager.cs
+++ Assets/Scripts/Results/ResultsManager.cs
@@ -8,22 +8,91 @@ namespace DDY_GJM_23
     // The results manager.
     public class ResultsManager : MonoBehaviour
     {
+        // The ResultsManager instance.
+        private static ResultsManager instance;
+
+        // Gets set to 'true' when the ResultsManager is initialized.
+        // This isn't needed, but it helps with the clarity.
+        private bool initialized = false;
+
         // The results data.
         public ResultsData results;
 
         // [Header("UI")]
 
+        // Constructor
+        private ResultsManager()
+        {
+            // ...
+        }
+
+        // Awake is called when the script is being loaded
+        protected virtual void Awake()
+        {
+            // If the instance hasn't been set, set it to this object.
+            if (instance == null)
+            {
+                instance = this;
+            }
+            // If the instance isn't this, destroy the game object.
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            // Run code for initialization.
+            if (!initialized)
+            {
+                initialized = true;
+            }
+        }
 
         // Start is called before the first frame update
         void Start()
         {
             // Finds the results data.
-            if(results == null)
+            if (results == null)
                 results = FindObjectOfType<ResultsData>();
 
             // Results were found, so load them up.
             if (results != null)
                 LoadResults();
+        }
+
+        // Gets the instance.
+        public static ResultsManager Instance
+        {
+            get
+            {
+                // Checks if the instance exists.
+                if (instance == null)
+                {
+                    // Tries to find the instance.
+                    instance = FindObjectOfType<ResultsManager>(true);
+
+
+                    // The instance doesn't already exist.
+                    if (instance == null)
+                    {
+                        // Generate the instance.
+                        GameObject go = new GameObject("ResultsManager (ResultsManager)");
+                        instance = go.AddComponent<ResultsManager>();
+                    }
+
+                }
+
+                // Return the instance.
+                return instance;
+            }
+        }
+
+        // Returns 'true' if the object has been initialized.
+        public bool Initialized
+        {
+            get
+            {
+                return initialized;
+            }
         }
 
         // Loads the results.
--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -2,17 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TitleManager : MonoBehaviour
+namespace DDY_GJM_23
 {
-    // Start is called before the first frame update
-    void Start()
+    // Manages the title screen.
+    public class TitleManager : MonoBehaviour
     {
-        
-    }
+        // The TitleManager instance.
+        private static TitleManager instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Gets set to 'true' when the TitleManager is initialized.
+        // This isn't needed, but it helps with the clarity.
+        private bool initialized = false;
+
+        // Constructor
+        private TitleManager()
+        {
+            // ...
+        }
+
+        // Awake is called when the script is being loaded
+        protected virtual void Awake()
+        {
+            // If the instance hasn't been set, set it to this object.
+            if (instance == null)
+            {
+                instance = this;
+            }
+            // If the instance isn't this, destroy the game object.
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            // Run code for initialization.
+            if (!initialized)
+            {
+                initialized = true;
+            }
+        }
+
+        // Gets the instance.
+        public static TitleManager Instance
+        {
+            get
+            {
+                // Checks if the instance exists.
+                if (instance == null)
+                {
+                    // Tries to find the instance.
+                    instance = FindObjectOfType<TitleManager>(true);
+
+
+                    // The instance doesn't already exist.
+                    if (instance == null)
+                    {
+                        // Generate the instance.
+                        GameObject go = new GameObject("TitleManager (singleton)");
+                        instance = go.AddComponent<TitleManager>();
+                    }
+
+                }
+
+                // Return the instance.
+                return instance;
+            }
+        }
+
+        // Returns 'true' if the object has been initialized.
+        public bool Initialized
+        {
+            get
+            {
+                return initialized;
+            }
+        }
     }
 }

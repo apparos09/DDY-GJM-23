4debe94fabf825a5ccbdb3ef11d12a17eef96eff
mecha-rm <roderick.montague243@gmail.com>
2023-07-10 16:09:20 -0400

    Added Prefabs for New Tile Images
    I added prefabs for the new tile images. I'm going to add to the area prefab script soon.

--- Assets/Scripts/Gameplay/Tiles/WorldTile.cs
+++ Assets/Scripts/Gameplay/Tiles/WorldTile.cs
@@ -8,7 +8,7 @@ namespace DDY_GJM_23
     public class WorldTile : AreaEntity
     {
         // The type of the tile. // TODO: add sand.
-        public enum tileType { none, grass, metal, pavement, brick, bridge, pit, water, poison };
+        public enum tileType { none, grass, metal, pavement, brick, bridge, pit, water, poison, sand };
 
         // The type of the world tile.
         public tileType type;

d79803b9e8dce594cd13fc358497fd3d4e355c39
Roderick Montague <roderick.montague243@gmail.com>
2023-07-11 22:22:27 -0400

    Updated the Portal Script
    Updated the script for the portal to account for an end portal being attached.

--- Assets/Scripts/Gameplay/Objects/Portal.cs
+++ Assets/Scripts/Gameplay/Objects/Portal.cs
@@ -12,7 +12,8 @@ namespace DDY_GJM_23
         // The gameplay manager.
         public GameplayManager gameManager;
 
-        // The object used to makr the destinati
+        // The object used to make the destination. If set to null, offset is used as the end position.
+        [Tooltip("The destination object. If set to null, offset is used as the destination.")]
         public GameObject destination;
 
         // Valid tags for the portal to use.
@@ -20,6 +21,21 @@ namespace DDY_GJM_23
 
         // The position offset.
         public Vector3 offset = Vector3.zero;
+
+        [Header("Portal/End Portal")]
+
+        // The ending portal.
+        public Portal endPortal;
+
+        // Uses the ignore next list.
+        [Tooltip("If 'true', the ignore next list is used. " +
+            "If the offset makes the object not collide with the end portal, set this to false.")]
+        public bool useIgnoreNextList = true;
+
+        // If set to 'true', the end portal ignores the next player collision.
+        // Use this to stop the end portal from triggering until the player gets off it.
+        [Tooltip("A list of objects to ignore next time they collide with this portal.")]
+        public List<GameObject> ignoreNextList = new List<GameObject>();
 
         // Start is called just before any of the Update methods is called the first time
         protected override void Start()
@@ -29,22 +45,42 @@ namespace DDY_GJM_23
             // The gameplay manager is not set, so set it.
             if(gameManager == null)
                 gameManager = GameplayManager.Instance;
+
+            // If the end portal is null, and the destination is not null.
+            if (endPortal == null && destination != null)
+                endPortal = destination.GetComponent<Portal>();
         }
 
         // OnCollisionEnter2D 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            // Valid tag.
-            if(validTags.Contains(collision.gameObject.tag))
-                Transport(collision.gameObject);   
+            // Checks if the object should be ignored.
+            if(ignoreNextList.Contains(collision.gameObject))
+            {
+                ignoreNextList.Remove(collision.gameObject);
+            }
+            else
+            {
+                // Valid tag.
+                if (validTags.Contains(collision.gameObject.tag))
+                    Transport(collision.gameObject);
+            } 
         }
 
         // OnTriggerEnter2D
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            // Valid tag.
-            if (validTags.Contains(collision.gameObject.tag))
-                Transport(collision.gameObject);
+            // Checks if the object should be ignored.
+            if (ignoreNextList.Contains(collision.gameObject))
+            {
+                ignoreNextList.Remove(collision.gameObject);
+            }
+            else
+            {
+                // Valid tag.
+                if (validTags.Contains(collision.gameObject.tag))
+                    Transport(collision.gameObject);
+            }
         }
 
         // Transports the user.
@@ -58,12 +94,22 @@ namespace DDY_GJM_23
             if (gameManager == null)
                 gameManager = GameplayManager.Instance;
 
+            // Get the base position.
+            Vector3 destPos = ((destination != null) ? destination.transform.position : Vector3.zero) + offset;
+
+            // Adds the object to the end portal's ignore next list. 
+            if(endPortal != null && useIgnoreNextList)
+            {
+                endPortal.ignoreNextList.Add(user);
+            }
+
             // Move to the destination.
-            user.transform.position = destination.transform.position + offset;
+            user.transform.position = destPos;
 
             // Activates all areas so that the triggers trigger.
-            // FIXME: this could probably be optimized.
-            gameManager.world.ActivateAllAreas();
+            // If the world isn't optimized, all colliders will be on regardless, so activating all areas isn't needed.
+            if(World.OPTIMIZE_WORLD)
+                gameManager.world.ActivateAllAreas();
 
         }
 
